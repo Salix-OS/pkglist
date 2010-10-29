06368801f7f537c70b76eda1f1017627180882bb
fredgnix <fredgnix@a8afeb8c-277c-4b4a-a4f6-ef4c85adc32b>
2010-10-29 17:15:18 +0000

    first import, make a naked webpage

---
+++ pkglist.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+#
+# This program is free software. It comes without any warranty, to
+# the extent permitted by applicable law. You can redistribute it
+# and/or modify it under the terms of the Do What The Fuck You Want
+# To Public License, Version 2, as published by Sam Hocevar. See
+# http://sam.zoy.org/wtfpl/COPYING for more details.
+#
+# Frédéric Galusik <fredg (AT) salixos (dot) org>
+#
+# pkglist.sh
+#
+
+REPO="http://salix.enialis.net/i486/13.1/source/"
+HTML="pkglist.html"
+TITLE="SalixOS Packages"
+
+printf "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\"
+  \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">
+<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" lang=\"en\">
+
+<head>
+    <title>$TITLE</title>
+    <meta http-equiv=\"content-type\" content=\"text/html;charset=utf-8\" />
+</head>
+
+<body>
+" > $HTML
+
+ls -d */* | sed -e s@^@$REPO@g | sort -f -t / -k 8 | \
+while IFS=/ read prot z rep arch rel src cat pkg
+    do
+        printf "%s: <a href=\"%s//%s/%s/%s/%s/%s/%s/SLKBUILD\">SLKBUILD</a> \
+- <a href=\"%s//%s/%s/%s/%s/%s/%s/\">source</a><br \>\n" \
+$pkg $prot $rep $arch $rel $src $cat $pkg $prot $rep $arch $rel $src $cat $pkg
+    done >> $HTML
+
+printf "
+</body>
+</html>" >> $HTML
+
+
